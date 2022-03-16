4bf9e0464f5d42dc907a3807fa227d1cd8b8e66e
Léo Colombaro <git@colombaro.fr>
2022-03-16 18:21:18 +0100

    Refactor config with separated files for navs

---
+++ .vitepress/config/navbar.ts
@@ -0,0 +1,44 @@
+import { NavItem } from '@vue/theme/src/vitepress/config'
+
+export const nav: NavItem[] = [
+  {
+    text: 'Docs',
+    link: '/guide/quick-start',
+    activeMatch: `^/(guide|style-guide|cookbook|examples)/`,
+  },
+  {
+    text: 'Development',
+    link: '/development/plugins',
+    activeMatch: `^/(development)/`,
+  },
+  {
+    text: 'Ecosystem',
+    items: [
+      {
+        text: 'Ressources',
+        items: [
+          {
+            text: 'Awesome YOURLS',
+            link: 'https://github.com/YOURLS/awesome-yourls',
+          },
+          {
+            text: 'Plugin sample',
+            link: 'https://github.com/YOURLS/plugin-sample',
+          },
+        ],
+      },
+      {
+        text: 'References',
+        items: [
+          { text: 'Hooklist', link: 'https://yourls.org/hooklist.php' },
+          { text: 'XRef', link: 'https://yourls.org/xref/' },
+        ],
+      },
+      {
+        text: 'News',
+        items: [{ text: 'Blog', link: 'https://blog.yourls.org' }],
+      },
+    ],
+  },
+  { text: 'Sponsor', link: '/sponsor' },
+]
