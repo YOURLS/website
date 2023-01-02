a546d0369c7991127bdb67eef8f4e46fb9925257
Léo Colombaro <git@colombaro.fr>
2023-01-02 21:18:47 +0100

    Add a serverless function for routing

---
+++ functions/[[catchall]].js
@@ -0,0 +1,18 @@
+export async function onRequest({ next, request }) {
+  const file = await next()
+
+  if (file.status === 404) {
+    const url = new URL(request.url)
+    url.hostname = `yourls.org`
+
+    const app = await fetch(url, {
+      redirect: 'manual',
+    })
+
+    if (app.status !== 404) {
+      return app
+    }
+  }
+
+  return file
+}
