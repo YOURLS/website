7eb03708377f52d6b32945a4e0b4c5ba1a21acc3
Léo Colombaro <git@colombaro.fr>
2023-01-02 21:07:21 +0100

    Switch engine to Docusaurus

---
+++ docs/guide/extend/possible-with-a-plugin.md
@@ -0,0 +1,35 @@
+# Possible with a plugin
+
+Not all feature requests can be implemented. Why?
+
+## Keeping things light
+
+Some issues here are feature requests or "is it possible to do this and that" requests.
+
+One of the primary goal of YOURLS is to achieve balance between features and efficiency. In other words, to keep it as light as possible while being powerful. In other words, to keep away from bloating core with features that won't be used by many.
+
+So, yes, feature requests are often closed with the **`"plugin domain"`** label.
+
+## Don't modify core files
+
+That's idiotic and will get you in trouble. [Don't hack core](/docs/development/dont-hack-core). Make plugins instead.
+
+## There is a plugin API for this
+
+Most of the time, features that are requested won't be implemented into core, but are easily implementable with a plugin. That's the whole point of the [plugin API](/docs/development/plugins) that has been introduced in YOURLS.
+
+## There may be already a plugin for this
+
+Head to <https://github.com/YOURLS/awesome-yourls> to check if someone already coded a plugin with this feature, or get that plugin coded by someone who coded a similar plugin.
+
+## Find a plugin coder
+
+If you can't code yourself and want a feature implemented, you need to find a plugin coder.
+
+An option is to head towards freelancer sites (getacoder.com, elance.com, etc.) and post your project. You should get very affordable quotes.
+
+Another option, that includes quality, full YOURLS code knowledge and future-proof coding, is to ask me (Ozh, contact me offsite by mail, ozh at ozh dot org) for a quote. Depending on the project and my free time, I may be able to help you.
+
+## Find plugin ideas
+
+If you can code (it's fun!) and look for ideas, browse through the issues labelled with [`plugin domain`](https://github.com/YOURLS/YOURLS/issues?q=label%3A%22plugin+domain%22+). You will find there people with real use case and needs!
---
+++ sidebars.js
@@ -0,0 +1,108 @@
+/**
+ * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
+ */
+
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  docs: [
+    {
+      label: 'Getting started',
+      type: 'category',
+      link: {
+        type: 'generated-index',
+      },
+      collapsed: false,
+      items: [
+        'guide/introduction',
+        'guide/install',
+        'guide/server-configuration',
+        'guide/upgrade',
+      ],
+    },
+    {
+      label: 'Essentials',
+      type: 'category',
+      link: {
+        type: 'generated-index',
+      },
+      items: [
+        'guide/essentials/configuration',
+        'guide/essentials/credentials',
+        'guide/essentials/private-or-public',
+        'guide/essentials/charset',
+      ],
+    },
+    {
+      label: 'Extend',
+      type: 'category',
+      link: {
+        type: 'generated-index',
+      },
+      items: [
+        'guide/extend/plugins',
+        'guide/extend/pages',
+        'guide/extend/languages',
+        'guide/extend/possible-with-a-plugin',
+      ],
+    },
+    {
+      label: 'Advanced',
+      type: 'category',
+      link: {
+        type: 'generated-index',
+      },
+      items: [
+        'guide/advanced/passwordless-api',
+        'guide/advanced/proxy',
+        'guide/advanced/public-shortening',
+        'guide/advanced/custom-protocols',
+      ],
+    },
+    {
+      label: 'Troubleshooting',
+      type: 'category',
+      link: {
+        type: 'generated-index',
+      },
+      items: [
+        'guide/troubleshooting/first-steps',
+        'guide/troubleshooting/frequent-issues',
+        'guide/troubleshooting/abuse',
+      ],
+    },
+  ],
+  dev: [
+    'development/plugins',
+    'development/dont-hack-core',
+    'development/coding-standards',
+    'development/hooks',
+    'development/database-queries',
+    'development/form-security',
+    'development/i18n',
+    'development/debugging',
+    {
+      label: 'Examples',
+      type: 'category',
+      link: {
+        type: 'generated-index',
+      },
+      items: [
+        'development/examples/qrcode',
+        'development/examples/preview',
+        'development/examples/public-prefix',
+        'development/examples/rss',
+      ],
+    },
+  ],
+}
+
+module.exports = sidebars
