0f08139cc150df330242b968dba4866d98940793
Léo Colombaro <git@colombaro.fr>
2023-01-04 01:11:52 +0100

    Use dedicated hostname for backend app

--- functions/[[catchall]].js
+++ functions/[[catchall]].js
@@ -3,7 +3,7 @@ export async function onRequest({ next, request }) {
 
   if (file.status === 404) {
     const url = new URL(request.url)
-    url.hostname = `yourls.org`
+    url.hostname = `app.yourls.org`
 
     const app = await fetch(url, {
       redirect: 'manual',
