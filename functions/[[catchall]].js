1f3c35419d2bc5220fd744e9e9eeeba1243cd976
Léo Colombaro <git@colombaro.fr>
2023-02-08 22:50:54 +0000

    Fix subrequests parameters

--- functions/[[catchall]].js
+++ functions/[[catchall]].js
@@ -5,9 +5,14 @@ export async function onRequest({ next, request }) {
     const url = new URL(request.url)
     url.hostname = `app.yourls.org`
 
-    const app = await fetch(url, {
-      redirect: 'manual',
-    })
+    const app = await fetch(
+      new Request(
+        url.toString(),
+        new Request(request, {
+          redirect: 'manual',
+        }),
+      ),
+    )
 
     if (app.status !== 404) {
       return app
